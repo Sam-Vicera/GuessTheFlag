225cfb6eac962e351255924ca188c249b2b0ff8a
Samuel Hernandez Vicera <shernandezvicera@apple.com>
2024-02-29 12:58:58 -0700

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Samuel Hernandez Vicera on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
